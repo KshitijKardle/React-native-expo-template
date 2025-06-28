fa16b1a77d05a2a87ab5a51f47690d3755ff2b7c
kshitij kardile <148033613+KshitijKardle@users.noreply.github.com>
2025-06-28 10:40:29 +0530

    initial commit

---
+++ Frontend-master/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: [require('nativewind/preset')],
+  content: [
+    './App.{js,jsx,ts,tsx}',
+    './app//*.{js,jsx,ts,tsx}',
+    './components//*.{js,jsx,ts,tsx}',
+  ],
+};0
